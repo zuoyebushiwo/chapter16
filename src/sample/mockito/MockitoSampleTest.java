a13556d8fb8355530f8234f19f394d0fc3328164
zhangxuej <957168943@qq.com>
2015-03-02 17:28:32 +0800

    Change-Id: I3ebbd4a1c79b1a8ce516e58ffa28a0a08e527a9b

--- src/sample/mockito/MockitoSampleTest.java
+++ src/sample/mockito/MockitoSampleTest.java
@@ -1,8 +1,45 @@
 package sample.mockito;
 
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.MockitoAnnotations;
+import org.mockito.MockitoAnnotations.Mock;
+
+import com.baobaotao.domain.User;
+import com.baobaotao.service.UserService;
+import com.baobaotao.service.UserServiceImpl;
+
+@SuppressWarnings("deprecation")
 public class MockitoSampleTest {
-	
+
 	// 模拟接口
-	
+	UserService mockUserService = mock(UserService.class);
+
+	// 模拟实现类
+	UserServiceImpl mockServiceImpl = mock(UserServiceImpl.class);
+
+	// 基于注释模拟类
+	@Mock
+	User mockUser;
+
+	@Before
+	public void initMocks() {
+		// 初始化当前测试类所有的@Mock注释模拟对象
+		MockitoAnnotations.initMocks(this);
+	}
+
+	/**
+	 * 模拟接口UserService测试
+	 */
+	@Test
+	public void testMockInterface() {
+		// 对方法设定返回值
+		when(mockUserService.findUserByUserName("tom")).thenReturn(
+				new User("tom", "1234"));
+		doReturn(true).when(mockUserService).hasMatchUser("tom", "1234"); 
+	}
 
 }
