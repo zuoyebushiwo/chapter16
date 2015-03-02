c72c6401a874a216de77ea5b2ae652c84801fb25
zhangxuej <957168943@qq.com>
2015-03-02 23:33:31 +0800

    Change-Id: I431936087ed3fde3f70842d4011081d1fada2496

---
+++ src/sample/unitils/spring/UserServiceTest.java
@@ -0,0 +1,25 @@
+package sample.unitils.spring;
+
+import static org.junit.Assert.assertNotNull;
+
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.unitils.UnitilsJUnit4;
+import org.unitils.spring.annotation.SpringApplicationContext;
+import org.unitils.spring.annotation.SpringBean;
+
+import com.baobaotao.service.UserService;
+
+public class UserServiceTest extends UnitilsJUnit4 {
+	@SpringApplicationContext({ "baobaotao-service.xml", "baobaotao-dao.xml" })
+	private ApplicationContext applicationContext;
+
+	@SpringBean("userService")
+	private UserService userService;
+
+	@Test
+	public void testLoadSpringBean() {
+		assertNotNull(applicationContext);
+		assertNotNull(userService.findUserByUserName("tom"));
+	}
+}
